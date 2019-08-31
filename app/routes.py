9816404f5f84b9b9e398552f08e154555221deb3
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-08-31 15:30:46 +0200

    Black slate again

--- app/routes.py
+++ app/routes.py
@@ -10,15 +10,6 @@ from werkzeug.contrib.fixers import ProxyFix
 app.wsgi_app = ProxyFix(app.wsgi_app)
 SSLify(app)
 
-# Use the application default credentials and connect db
-cred = credentials.ApplicationDefault()
-if 'PROJECT_ID' in os.environ:
-    print('Staging area: ' + os.environ['PROJECT_ID'])
-    firebase_admin.initialize_app(cred, {
-        'projectId': os.environ['PROJECT_ID']
-    })
-    db = firestore.client()
-
 # Overview of all the routes
 @app.route('/')
 @app.route('/index')
--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Starting the engines!
-exec gunicorn -b 0.0.0.0:$PORT
+exec gunicorn -b 0.0.0.0:$PORT --access-logfile - --error-logfile - de-uitdaging:app
