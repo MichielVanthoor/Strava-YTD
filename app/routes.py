764db1b02fe03f5336376259f2bcbba1bf2d5471
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-08-31 15:22:21 +0200

    Updating Gunicorn

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,7 @@
 from app import app
 
-import firebase_admin
 import os
 
-from firebase_admin import credentials
-from firebase_admin import firestore
 from flask import render_template, request
 from flask_sslify import SSLify
 from werkzeug.contrib.fixers import ProxyFix
@@ -22,21 +19,8 @@ if 'PROJECT_ID' in os.environ:
     })
     db = firestore.client()
 
-
 # Overview of all the routes
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-
-@app.route('/over_ons')
-def about_us():
-    return render_template('about_us.html')
-
-
-@app.route('/projects')
-def projects():
-    projects = db.collection(u'projects').order_by(u'date').steam()
-
-    return render_template('projects.html', projects=projects)
