25451164d650816054685cce0ed565eb03044ad6
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-08-31 14:55:22 +0200

    Initial boiler plate

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python
+
+RUN useradd -ms /bin/bash strava-ytd
+
+WORKDIR /home/strava-ytd
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY app app
+COPY strava-ytd.py boot.sh ./
+RUN chmod +x boot.sh
+
+ENV FLASK_APP strava-ytd.py
+
+RUN chown -R strava-ytd:strava-ytd ./
+USER strava-ytd
+
+EXPOSE 5000
+ENTRYPOINT ["./boot.sh"]
---
+++ app/routes.py
@@ -0,0 +1,42 @@
+from app import app
+
+import firebase_admin
+import os
+
+from firebase_admin import credentials
+from firebase_admin import firestore
+from flask import render_template, request
+from flask_sslify import SSLify
+from werkzeug.contrib.fixers import ProxyFix
+
+# Configure HTTPS redirect
+app.wsgi_app = ProxyFix(app.wsgi_app)
+SSLify(app)
+
+# Use the application default credentials and connect db
+cred = credentials.ApplicationDefault()
+if 'PROJECT_ID' in os.environ:
+    print('Staging area: ' + os.environ['PROJECT_ID'])
+    firebase_admin.initialize_app(cred, {
+        'projectId': os.environ['PROJECT_ID']
+    })
+    db = firestore.client()
+
+
+# Overview of all the routes
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/over_ons')
+def about_us():
+    return render_template('about_us.html')
+
+
+@app.route('/projects')
+def projects():
+    projects = db.collection(u'projects').order_by(u'date').steam()
+
+    return render_template('projects.html', projects=projects)
