7bb339fc15bdb02a6a67bfa3eca4b2332a0cf0b8
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-09-03 19:52:47 +0200

    Implementing Strava API

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,30 @@
 from app import app
 
+
 import os
 
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 from flask_sslify import SSLify
+from stravalib.client import Client
 from werkzeug.contrib.fixers import ProxyFix
 
 # Configure HTTPS redirect
 app.wsgi_app = ProxyFix(app.wsgi_app)
 SSLify(app)
+stravaclient = Client()
+
 
 # Overview of all the routes
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return redirect(stravaclient.authorization_url(client_id=29215, redirect_uri= url_for('authorized')))
+
+@app.route('/authorized')
+def authorized():
+    return render_template('index.html')
+
