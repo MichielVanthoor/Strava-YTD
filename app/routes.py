67486cf7cd78aeef7493f3e7d20542ac5c8cee7c
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-09-04 13:27:57 +0200

    Adding the authorized plane

--- app/routes.py
+++ app/routes.py
@@ -27,26 +27,27 @@ def login():
 
 @app.route('/authorized')
 def authorized():
-	code = request.request.args.get('code')
-	token_response = client.exchange_code_for_token(client_id=29215, client_secret='9f09fa714a5aa4f9a0950b56ccc2f9d008ce2102', code=code)
-    access_token = token_response['access_token']
-    refresh_token = token_response['refresh_token']
-    expires_at = token_response['expires_at']    
+    # code = request.request.args.get('code')
+    # token_response = client.exchange_code_for_token(client_id=29215, client_secret='9f09fa714a5aa4f9a0950b56ccc2f9d008ce2102', code=code)
+    # access_token = token_response['access_token']
+    # refresh_token = token_response['refresh_token']
+    # expires_at = token_response['expires_at']    
 
-    # Now store that short-lived access token somewhere (a database?)
-    client.access_token = access_token
-    # You must also store the refresh token to be used later on to obtain another valid access token 
-    # in case the current is already expired
-    client.refresh_token = refresh_token    
+    # # Now store that short-lived access token somewhere (a database?)
+    # client.access_token = access_token
+    # # You must also store the refresh token to be used later on to obtain another valid access token 
+    # # in case the current is already expired
+    # client.refresh_token = refresh_token    
 
-    # An access_token is only valid for 6 hours, store expires_at somewhere and
-    # check it before making an API call.
-    client.token_expires_at = expires_at
+    # # An access_token is only valid for 6 hours, store expires_at somewhere and
+    # # check it before making an API call.
+    # client.token_expires_at = expires_at
      
-    athlete = client.get_athlete()
-    print("For {id}, I now have an access token {token}".format(id=athlete.id, token=access_token))
+    # athlete = client.get_athlete()
+    # print("For {id}, I now have an access token {token}".format(id=athlete.id, token=access_token))
 
-    name=athlete.id    
+    # name=athlete.id
+    name='Michiel'    
 
     return render_template('authorized.html', name=name)
 
